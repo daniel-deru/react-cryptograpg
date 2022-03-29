7f7e9aa534c43ba05ee8dd9f3545836df5128bfe
daniel <danielderu@gmail.com>
2022-03-29 21:45:53 +0200

    added cryptjs for encryptions

--- src/pages/styles/encode.styled.ts
+++ src/pages/styles/encode.styled.ts
@@ -4,67 +4,72 @@ export const EncodePage = styled.section`
     color: ${({theme}) => theme.colors.green};
 
     main {
-        display: flex;
-        justify-content: space-evenly;
-        margin-top: 4rem;
-
-        div {
-            font-size: 2em;
+        .input-container {
             display: flex;
-            flex-direction: column;
-            width: 40%;
+            justify-content: space-evenly;
+            margin-top: 4rem;
 
-            input {
-                background-color: black;
-                color: ${({theme}) => theme.colors.green};
-                font-family: ${({theme}) => theme.fonts.retro};
-                font-size: 0.75em;
-                border-radius: 5px;
-                border: 2px dashed ${({theme}) => theme.colors.darkGreen};
-                outline: none;
-                padding: 5px;
-                width: 100%;
-                &:focus {
-                    border: 2px dashed ${({theme}) => theme.colors.green};
+            div {
+                font-size: 2em;
+                display: flex;
+                flex-direction: column;
+                width: 40%;
+
+                input {
+                    background-color: black;
+                    color: ${({theme}) => theme.colors.green};
+                    font-family: ${({theme}) => theme.fonts.retro};
+                    font-size: 0.75em;
+                    border-radius: 5px;
+                    border: 2px dashed ${({theme}) => theme.colors.darkGreen};
+                    outline: none;
+                    padding: 5px;
+                    width: 100%;
+                    &:focus {
+                        border: 2px dashed ${({theme}) => theme.colors.green};
+                    }
                 }
+
+                textarea {
+                    width: 40vw;
+                    max-width: 40vw;
+                    min-width: 40vw;
+                    max-height: 50vh;
+                    min-height: calc(0.75em + 18px);
+                    background-color: black;
+                    color: ${({theme}) => theme.colors.green};
+                    font-family: ${({theme}) => theme.fonts.retro};
+                    font-size: 0.75em;
+                    border-radius: 5px;
+                    border: 2px dashed ${({theme}) => theme.colors.darkGreen};
+                    outline: none;
+                    padding: 5px;
+                    width: 100%;
+                    &:focus {
+                        border: 2px dashed ${({theme}) => theme.colors.green};
+                    }
+                } 
             }
-
-            textarea {
-                width: 40vw;
-                max-width: 40vw;
-                min-width: 40vw;
-                max-height: 50vh;
-                min-height: calc(0.75em + 18px);
-                background-color: black;
-                color: ${({theme}) => theme.colors.green};
-                font-family: ${({theme}) => theme.fonts.retro};
-                font-size: 0.75em;
-                border-radius: 5px;
-                border: 2px dashed ${({theme}) => theme.colors.darkGreen};
-                outline: none;
-                padding: 5px;
-                width: 100%;
-                &:focus {
-                    border: 2px dashed ${({theme}) => theme.colors.green};
-                }
-            } 
         }
+
+        button {
+            background-color: black;
+            color: ${({theme}) => theme.colors.green};
+            font-family: ${({theme}) => theme.fonts.retro};
+            margin: 2rem;
+            font-size: 2em;
+            padding: 5px 30px;
+            border: 2px inset ${({theme}) => theme.colors.green};
+            cursor: pointer;
+            border-radius: 5px;
+
+            &:active {
+                border: 2px outset ${({theme}) => theme.colors.green};
+            }
+    }
+        
     }
 
-    button {
-        background-color: black;
-        color: ${({theme}) => theme.colors.green};
-        font-family: ${({theme}) => theme.fonts.retro};
-        margin: 2rem;
-        font-size: 2em;
-        padding: 5px 30px;
-        border: 2px inset ${({theme}) => theme.colors.green};
-        cursor: pointer;
-        border-radius: 5px;
-
-        &:active {
-            border: 2px outset ${({theme}) => theme.colors.green};
-        }
-    }
+    
 
 `
--- src/pages/tsx/encode.tsx
+++ src/pages/tsx/encode.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react'
-
+import { useEffect, useRef } from 'react'
+import CryptoJS from 'crypto-js'
 import { useAppDispatch, useAppSelector } from '../../store'
 import Header from '../../components/tsx/Header'
 import { EncodePage } from '../styles/encode.styled'
@@ -11,6 +11,25 @@ const Encode: React.FC = () => {
   const codeType = useAppSelector(state => state.type)
   const encryptionMethod = useAppSelector(state => state.encryption)
 
+  const messageRef = useRef<HTMLTextAreaElement | null>(null)
+  const keyRef = useRef<HTMLInputElement | null>(null)
+
+  const getMessage = (): void => {
+    const message: string | undefined = messageRef.current?.value
+    const key: string | undefined = keyRef.current?.value
+
+    let encryptedMessage: CryptoJS.lib.CipherParams | string
+
+    if(key && message) {
+      encryptedMessage = CryptoJS.DES.encrypt(message, key).toString()
+      console.log("This is the encrypted message: ", encryptedMessage)
+      console.log("This is the decrypted message: ", CryptoJS.DES.decrypt(encryptedMessage, key).toString())
+    } else {
+      console.log(message)
+    }
+
+  }
+
   useEffect(() => {
     console.log(encryptionMethod)
   }, [codeType, encryptionMethod])
@@ -18,16 +37,22 @@ const Encode: React.FC = () => {
     <EncodePage>
       <Header />
       <main>
-        <div>
-          <label htmlFor="">Enter Your Message</label>
-          <textarea name="" id=""></textarea>
+        <div className='input-container'>
+          <div>
+            <label htmlFor="">Enter Your Message</label>
+            <textarea name="" id="" ref={messageRef}></textarea>
+          </div>
+          <div>
+            <label htmlFor="">Enter Your Key</label>
+            <input type="text" ref={keyRef}/>
+          </div>
         </div>
-        <div>
-          <label htmlFor="">Enter Your Key</label>
-          <input type="text" />
-        </div>
+        <button onClick={() => getMessage()}>{codeType == ENCODE ? "Encode" : "Decode"}</button>
       </main>
-      <button>{codeType == ENCODE ? "Encode" : "Decode"}</button>
+     
+      <section>
+        
+      </section>
     </EncodePage> 
   )
 }
